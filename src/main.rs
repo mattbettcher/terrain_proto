f862f7c1bc75b03822183fdf69a277e4eb8f0ea3
Matthew Bettcher <mattbettcher@gmail.com>
2021-12-14 08:50:43 -0500

    Updated to use @eweilnau code

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,6 @@ fn main() {
     app.add_startup_system(setup.system()).run();
 }
 
-const MAP_ORIGIN_X: f32 = 0.0;
-const MAP_ORIGIN_Y: f32 = 0.0;
-const TILE_SIZE_X: f32 = 50.0;
-const TILE_SIZE_Y: f32 = 24.0;
-
 fn move_camera(mut query: Query<(&Camera, &mut Transform)>)
 {
     if let Ok((_cam, mut transform)) = query.single_mut() {
@@ -33,27 +28,60 @@ fn setup(
     let texture_handle = asset_server.load("iso_tile_atlas.png");
     let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(64.0, 64.0), 2, 2);
     let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    let map = Map::new(texture_atlas_handle, 2, Vec2::new(50.0, 24.0));
+    map.spawn_tiles(&mut commands);
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    
-    // create a layer of tiles
-    for y in (0..32).rev() {
-        for x in (0..30).rev() {
-            
-            commands.spawn_bundle(SpriteSheetBundle {
-                sprite: TextureAtlasSprite {
-                    color: Color::GREEN,
-                    index: ((x as f32).sin() * 10.0 + (y as f32).cos() * 10.0) as u32 % 3,
-                    ..Default::default()
-                },
-                transform: Transform::from_translation(Vec3::from((to_screen(x as f32, y as f32), 0.0))),
-                texture_atlas: texture_atlas_handle.clone(),
-                ..Default::default()
-            });
-        }
-    }
 }
 
-fn to_screen(x: f32, y: f32) -> Vec2 {
-    Vec2::new(MAP_ORIGIN_X * TILE_SIZE_X + (x - y) * TILE_SIZE_X / 2.0,
-              MAP_ORIGIN_Y * TILE_SIZE_Y + (x + y) * TILE_SIZE_Y / 2.0)
+struct Map {
+    atlas: Handle<TextureAtlas>,
+    heightmap: Vec<u32>,
+    tile_columns: usize,
+    tile_size: Vec2,
+}
+
+impl Map {
+    fn new(atlas: Handle<TextureAtlas>, map_size: u32, tile_size: Vec2) -> Self {
+        let tile_columns = 2usize.pow(map_size) + 1;
+        let heightmap = vec![0; tile_columns * tile_columns];
+        Self {
+            atlas,
+            heightmap,
+            tile_columns,
+            tile_size,
+        }
+    }
+
+    fn spawn_tiles(&self, commands: &mut Commands) {
+        let tile_sprites: Vec<SpriteSheetBundle> = self
+            .heightmap
+            .iter()
+            .enumerate()
+            .map(|(index, &height)| SpriteSheetBundle {
+                sprite: TextureAtlasSprite {
+                    color: Color::GREEN,
+                    index: height,
+                    ..Default::default()
+                },
+                texture_atlas: self.atlas.clone(),
+                transform: Transform::from_translation(self.iso_coords(index)),
+                ..Default::default()
+            })
+            .collect();
+        commands.spawn_batch(tile_sprites);
+    }
+
+    fn iso_coords(&self, tile_index: usize) -> Vec3 {
+        let map_x = tile_index % self.tile_columns;
+        let map_y = tile_index / self.tile_columns;
+        let draw_order = map_x + map_y;
+
+        let cart_x = map_x as f32 - (self.tile_columns as f32 / 2.0);
+        let cart_y = (self.tile_columns as f32 / 2.0) - map_y as f32;
+
+        let iso_x = (cart_x + cart_y) / -2.0;
+        let iso_y = (cart_x - cart_y) / -2.0;
+
+        Vec3::new(iso_x * self.tile_size.x, iso_y * self.tile_size.y, draw_order as f32)
+    }
 }
